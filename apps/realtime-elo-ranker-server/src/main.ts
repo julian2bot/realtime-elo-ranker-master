f3fca0fddc417032f33e4f3536e3faf341bf767c
julian marques <marquesjulian26@gmail.com>
2026-01-13 18:04:21 +0100

    swagger

--- apps/realtime-elo-ranker-server/src/main.ts
+++ apps/realtime-elo-ranker-server/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Realtime ELO Ranker')
+    .setDescription('API documentation for the Realtime ELO Ranker')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+  
+  
   await app.listen(process.env.PORT ?? 8888);
+
+
 }
 bootstrap();
